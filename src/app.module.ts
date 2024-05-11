c24cf352e8c3bfb6125e6a172cf5071fea3519dd
Himanshu-AT <aristech.official@gmail.com>
2024-05-11 21:27:20 +0530

    setup init server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,44 @@
-import { Module } from '@nestjs/common';
+import { Module, Scope } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { TerminusModule } from '@nestjs/terminus';
+import { HttpModule } from '@nestjs/axios';
+import Redis from 'ioredis';
+import { MulterModule } from '@nestjs/platform-express/multer';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+// import { JwtStrategy } from './auth/jwt.strategy';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule,
+    TerminusModule,
+    HttpModule,
+    MulterModule.register({
+      dest: './uploads',
+    }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.EXPIRES_IN },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'REDIS',
+      useFactory: () => {
+        const client = new Redis(process.env.REDDIS_URL);
+        client.on('error', (err) => console.error('Redis error', err));
+        return client;
+      },
+      scope: Scope.DEFAULT,
+    },
+    // JwtStrategy,
+  ],
+  // exports: [AuthService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
+  app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle('Your API Title')
+    .setDescription('API documentation for Your API')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
