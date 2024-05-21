4d33f6cdd0f599ecea01522228452516c90b326d
Himanshu-AT <aristech.official@gmail.com>
2024-05-21 12:08:45 +0530

    resolve Add auth routes #1

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -2,25 +2,45 @@ import {
   ForbiddenException,
   HttpException,
   HttpStatus,
+  Inject,
   Injectable,
+  NotFoundException,
 } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import SignUpDto from './dto/signup.dto';
 import SignInDto from './dto/signin.dto';
 import handleErrors from 'src/handlers/handleErrors.global';
 import { JwtService } from '@nestjs/jwt';
+import sendEmail from 'src/handlers/email.global';
+import Redis from 'ioredis';
+import { emit } from 'process';
+import RetrieveInfoFromRequest from 'src/handlers/retriveInfoFromRequest.global';
 @Injectable()
 export class AuthService {
   constructor(
     private prisma: PrismaService,
+    @Inject('REDIS') private redisClient: Redis,
     private jwtService: JwtService,
   ) {}
+
+  async sendVerificationCode(
+    email: string,
+    subject: string,
+    text: string,
+  ): Promise<boolean> {
+    try {
+      const res = await sendEmail(email, subject, text);
+      return res;
+    } catch (error) {
+      return false;
+    }
+  }
 
   getHello(): string {
     return 'Hello from Auth Service!';
   }
 
-  async signUp(body: SignUpDto, response) {
+  async signUp(body: SignUpDto) {
     const { name, email, password, userName } = body;
 
     if (!userName || !email || !password || !name) {
@@ -43,8 +63,9 @@ export class AuthService {
       throw new ForbiddenException('Invalid email format');
     }
 
+    //! @Himasnhu-AT Improve token generation logic
     const token = Math.random().toString(36).substring(2);
-    const user = await this.prisma.user
+    let user = await this.prisma.user
       .create({
         data: {
           token,
@@ -59,6 +80,41 @@ export class AuthService {
         handleErrors(error);
       });
 
+    //! @Himasnhu-AT Improve verification Code generation logic
+    const verificationCode = Math.random().toString(36).substring(2);
+
+    try {
+      await this.redisClient.set(email, verificationCode);
+    } catch (error) {
+      return error;
+    } finally {
+      const subject = 'Email Verification';
+      const text = `Your verification code is ${verificationCode}`;
+      await this.sendVerificationCode(email, subject, text);
+    }
+
+    delete (user as { password: string }).password;
+    return user;
+  }
+
+  async verifyEmail(body: { email: string; token: string }, response) {
+    const verificationCode = await this.redisClient.get(body.email);
+
+    if (verificationCode !== body.token) {
+      throw new HttpException('Invalid token', HttpStatus.UNAUTHORIZED);
+    }
+
+    await this.redisClient.del(body.email);
+
+    let user = await this.prisma.user.update({
+      where: {
+        email: body.email,
+      },
+      data: {
+        isVerified: true,
+      },
+    });
+
     const payload = {
       sub: (user as { id: string }).id,
       email: (user as { email: string }).email,
@@ -70,9 +126,122 @@ export class AuthService {
 
     // Set the access_token as a cookie
     response.cookie('access_token', access_token, { httpOnly: true });
+    return { message: 'Email verified successfully' };
+  }
 
-    delete (user as { password: string }).password;
-    return user;
+  async forgotPassword(email: string) {
+    await this.prisma.user
+      .findUnique({
+        where: {
+          email: email,
+        },
+      })
+      .catch((error) => {
+        handleErrors(error);
+      });
+
+    //! @Himasnhu-AT Improve verification Code generation logic
+    const verificationCode = Math.random().toString(36).substring(2);
+
+    if (await this.redisClient.get(email)) {
+      await this.redisClient.del(email);
+    }
+
+    try {
+      await this.redisClient.set(email, verificationCode);
+    } catch (error) {
+      return error;
+    } finally {
+      const subject = 'Email Verification';
+      const text = `Your verification code is ${verificationCode}`;
+      await this.sendVerificationCode(email, subject, text);
+    }
+  }
+
+  async forgotPasswordEmailVerify(body: {
+    email: string;
+    token: string;
+    newPassword: string;
+  }) {
+    const verificationCode = await this.redisClient
+      .get(body.email)
+      .catch((error) => {
+        throw new NotFoundException('Token not found for given Email');
+      });
+
+    if (body.token != verificationCode) {
+      throw new ForbiddenException('Wrong verification Code.');
+    }
+
+    //! @Himasnhu-AT Hash this password
+    await this.prisma.user.update({
+      where: {
+        email: body.email,
+      },
+      data: {
+        password: body.newPassword,
+      },
+    });
+
+    return `Password changed successfully!`;
+  }
+
+  async updatePassword(
+    dto: { newPassword: string; oldPassword: string },
+    request: any,
+  ) {
+    const { id, email } = RetrieveInfoFromRequest(request);
+
+    const user = await this.prisma.user
+      .findUnique({
+        where: {
+          id,
+          email,
+        },
+      })
+      .catch((error) => {
+        handleErrors(error);
+      });
+
+    if (dto.oldPassword != (user as unknown as any).password) {
+      await this.prisma.user
+        .update({
+          where: {
+            id,
+            email,
+          },
+          data: {
+            password: dto.newPassword,
+          },
+        })
+        .catch((error) => {
+          handleErrors(error);
+        });
+    }
+
+    return `Password updated successfully!`;
+  }
+
+  async signoutUser(id, email, response) {
+    // const { id, email } = RetrieveInfoFromRequest(request);
+
+    await this.prisma.user
+      .update({
+        where: {
+          id,
+          email,
+        },
+        data: {
+          token: null,
+        },
+      })
+      .catch((error) => {
+        handleErrors(error);
+      });
+
+    response.cookie('access_token', '', { httpOnly: true });
+
+    return `SignOut Successful!`;
   }
 
   async signIn(body: SignInDto, response) {
@@ -86,10 +255,14 @@ export class AuthService {
       );
     }
 
+    const token = Math.random().toString(36).substring(2);
     const user = await this.prisma.user
-      .findUnique({
+      .update({
         where: {
           email,
+        },
+        data: {
+          token,
         },
       })
       .catch((error) => {
