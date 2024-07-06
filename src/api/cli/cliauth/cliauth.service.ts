ca41f6c916c2062644fcace4dffb1c5057998092
Himanshu-AT <aristech.official@gmail.com>
2024-07-07 00:07:22 +0530

    complete demo of cliauth

--- src/api/cli/cliauth/cliauth.service.ts
+++ src/api/cli/cliauth/cliauth.service.ts
@@ -40,25 +40,103 @@ export class CliauthService {
     return { cliToken: 'cliToken' };
   }
 
-  logout(authHeader: string) {
+  async logout(authHeader: string) {
     if (!authHeader) {
       throw new Error('No authorization header provided');
     }
 
-    const verified = this.prisma.user.update({
-            where: { 
-                cliToken: authHeader
-             },
-            data: { cliToken: null },
-        },
-        );
-    )
+    const info = authHeader.split('?userId=');
+
+    const token = info[0];
+    const userId = info[1];
+
+    const verified = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!verified) {
+      throw new Error('Invalid user');
+    }
+
+    if (token !== verified.tokenCli) {
+      throw new Error('Invalid token');
+    }
+
+    await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        tokenCli: null,
+      },
+    });
+
+    return 'Successfully logged out';
   }
 
-  refresh(authHeader: string) {
-    throw new Error('Method not implemented.');
+  async refresh(authHeader: string) {
+    if (!authHeader) {
+      throw new Error('No authorization header provided');
+    }
+
+    const info = authHeader.split('?userId=');
+
+    const token = info[0];
+    const userId = info[1];
+
+    const verified = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!verified) {
+      throw new Error('Invalid user');
+    }
+
+    if (token !== verified.tokenCli) {
+      throw new Error('Invalid token');
+    }
+
+    //! GENERATE NEW CliToken
+    await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        tokenCli: 'newToken',
+      },
+    });
+
+    return 'Successfully logged out';
   }
-  verify(authHeader: string) {
-    throw new Error('Method not implemented.');
+
+  async verify(authHeader: string) {
+    if (!authHeader) {
+      throw new Error('No authorization header provided');
+    }
+
+    const info = authHeader.split('?userId=');
+
+    const token = info[0];
+    const userId = info[1];
+
+    const verified = await this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!verified) {
+      throw new Error('Invalid user');
+    }
+
+    if (token !== verified.tokenCli) {
+      throw new Error('Invalid token');
+    }
+
+    return 'User available';
   }
 }
